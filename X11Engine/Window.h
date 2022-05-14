02941ba7963809b4f69843fc4423f1d67ba3d0d9
Антон Побылица <apobylitsa@GMAIL.COM>
2022-05-14 19:17:33 +0300

    Transferred to cmake

--- X11Engine/Window.h
+++ X11Engine/Window.h
@@ -1,10 +1,35 @@
 #pragma once
 #include <Windows.h>
-#include "Graphics.h"
+
 
 class Window
 {
 public:
+	Window() {
+		WNDCLASS wnd = {};
+		wnd.hCursor = LoadCursor(NULL, IDC_ARROW);
+		wnd.hInstance = NULL;
+		wnd.lpfnWndProc = Window::WndProc;
+		wnd.lpszClassName = "WindowClass";
+		wnd.hbrBackground = (HBRUSH)COLOR_WINDOW;
+		wnd.style = CS_HREDRAW | CS_VREDRAW;
+
+		RegisterClass(&wnd);
+
+		RECT rect;
+		rect.left = 0;
+		rect.top = 0;
+		rect.right = 1920;
+		rect.bottom = 1080;
+
+		m_hWnd = CreateWindow(wnd.lpszClassName, "", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, rect.right - rect.left, rect.bottom - rect.top, HWND_DESKTOP, NULL, NULL, NULL);
+	}
+
+	static Window& Get() {
+		static Window instance;
+		return instance;
+	}
+
 	static LRESULT WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 		switch (msg) {
 		case WM_DESTROY:
@@ -15,52 +40,21 @@ public:
 		}
 		return 0;
 	}
+
+	void Run() {
+		m_message = {};
+		while (m_message.message != WM_QUIT) {
+			if (PeekMessage(&m_message, NULL, NULL, NULL, PM_REMOVE)) {
+				TranslateMessage(&m_message);
+				DispatchMessage(&m_message);
+			}
+		}
+	}
+
+	inline HWND GetHandle() const { return m_hWnd; }
 private:
+	HWND m_hWnd;
+	MSG m_message;
 	int m_width;
 	int m_height;
 };
-
-int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-	WNDCLASS wnd = {};
-	wnd.hCursor = LoadCursor(hInstance, IDC_ARROW);
-	wnd.hInstance = hInstance;
-	wnd.lpfnWndProc = Window::WndProc;
-	wnd.lpszClassName = L"WindowClass";
-	wnd.hbrBackground = (HBRUSH)COLOR_WINDOW;
-	wnd.style = CS_HREDRAW | CS_VREDRAW;
-
-	RegisterClass(&wnd);
-
-	RECT rect;
-	rect.left = 0;
-	rect.top = 0;
-	rect.right = 1920;
-	rect.bottom = 1080;
-
-	HWND hWnd = CreateWindow(wnd.lpszClassName, L"", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 0, 0, rect.right - rect.left, rect.bottom - rect.top, HWND_DESKTOP, NULL, hInstance, NULL);
-
-	Graphics g{ 1920, 1080, hWnd };
-
-	vector<float> vert = {
-		0.5f, 0.5f, 0.5f,
-		-0.5f, 0.5f, 0.5f,
-		0.5f, -0.5f, 0.5f,
-		-0.5f, -0.5f, 0.5f
-	};
-	vector<uint32_t> ind = { 
-		0, 2, 1,
-		1, 2, 3
-	};
-
-	MSG message = {};
-	while (message.message != WM_QUIT) {
-		if (PeekMessage(&message, HWND_DESKTOP, NULL, NULL, PM_REMOVE)) {
-			TranslateMessage(&message);
-			DispatchMessage(&message);
-		}
-		g.PreFrame();
-		g.Draw(vert, ind);
-		g.PostFrame();
-	}
-	return 0;
-}
