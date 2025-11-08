f5ef3b56c44c74b163b2f17a166172fc00bee0f7
Leer-err <apobylitsa@gmail.com>
2025-11-09 02:36:40 +0300

    Singleton AnimationRegistry

--- X11Engine/src/Assets/Animation/AnimationRegistry.h
+++ X11Engine/src/Assets/Animation/AnimationRegistry.h
@@ -8,10 +8,23 @@
 
 class AnimationRegistry {
    public:
+    static AnimationRegistry& get() {
+        static AnimationRegistry instance;
+        return instance;
+    }
+
     void add(const Animation& animation);
     const Animation* get(const std::string& name) const;
 
    private:
+    AnimationRegistry() {}
+
+    AnimationRegistry(const AnimationRegistry&) = delete;
+    AnimationRegistry(AnimationRegistry&&) = delete;
+
+    AnimationRegistry& operator=(const AnimationRegistry&) = delete;
+    AnimationRegistry& operator=(AnimationRegistry&&) = delete;
+
     std::unordered_map<std::string, Animation> animations;
 };
 
