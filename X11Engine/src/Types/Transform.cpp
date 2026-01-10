05ec924a28e2fa2c4a23092c445582be8c627a78
Leer-err <apobylitsa@gmail.com>
2026-01-10 18:33:11 +0300

    Updated transform for storing global transform

--- X11Engine/src/Types/Transform.cpp
+++ X11Engine/src/Types/Transform.cpp
@@ -4,55 +4,73 @@
 
 #include "Matrix.h"
 
-Transform::Transform() { scale = Vector3(1, 1, 1); }
-
-void Transform::setPosition(const Vector3& position) {
-    this->position = position;
-    dirty_matrix = true;
+Transform::Transform() {
+    parent_matrix = Matrix::identity();
+    local_scale = Vector3(1, 1, 1);
+    scale = local_scale;
 }
 
-void Transform::setOrientation(const Quaternion& orientation) {
-    this->orientation = orientation;
-    dirty_matrix = true;
+void Transform::setLocalPosition(const Vector3& position) {
+    local_position = position;
+    dirty_globals = true;
+    dirty_local_matrix = true;
 }
 
-void Transform::setScale(const Vector3& scale) {
-    this->scale = scale;
-    dirty_matrix = true;
+void Transform::setLocalOrientation(const Quaternion& orientation) {
+    local_orientation = orientation;
+    dirty_globals = true;
+    dirty_local_matrix = true;
 }
 
-Vector3 Transform::getPosition() const { return position; }
+void Transform::setLocalScale(const Vector3& scale) {
+    local_scale = scale;
+    dirty_globals = true;
+    dirty_local_matrix = true;
+}
 
-Quaternion Transform::getOrientation() const { return orientation; }
+void Transform::setParentTransformMatrix(const Matrix& parent_global_matrix) {
+    parent_matrix = parent_global_matrix;
+    dirty_globals = true;
+}
 
-Vector3 Transform::getScale() const { return scale; }
+Vector3 Transform::getPosition() {
+    if (dirty_globals) updateGlobals();
+    return position;
+}
+
+Quaternion Transform::getOrientation() {
+    if (dirty_globals) updateGlobals();
+    return orientation;
+}
+
+Vector3 Transform::getScale() {
+    if (dirty_globals) updateGlobals();
+    return scale;
+}
+
+Vector3 Transform::getLocalPosition() { return local_position; }
+
+Quaternion Transform::getLocalOrientation() { return local_orientation; }
+
+Vector3 Transform::getLocalScale() { return local_scale; }
 
 Matrix Transform::getLocalMatrix() {
-    if (dirty_matrix == false) return local_matrix;
-
-    Matrix translation = Matrix::translation(position);
-    Matrix rotation = Matrix::rotation(orientation);
-    Matrix scale_matrix = Matrix::scale(scale);
-
-    local_matrix = scale_matrix * rotation * translation;
-    dirty_matrix = false;
+    if (dirty_local_matrix) updateLocalMatrix();
 
     return local_matrix;
 }
 
-Vector3 Transform::getForward() const {
-    return Vector3(0, 0, 1).rotate(orientation);
+Matrix Transform::getGlobalMatrix() {
+    if (dirty_globals) updateGlobals();
+
+    return global_matrix;
 }
 
-Vector3 Transform::getRight() const {
-    return Vector3(1, 0, 0).rotate(orientation);
-}
+Vector3 Transform::getForward() { return Vector3(0, 0, 1).rotate(orientation); }
 
-Vector3 Transform::getUp() const {
-    return Vector3(0, 1, 0).rotate(orientation);
-}
+Vector3 Transform::getRight() { return Vector3(1, 0, 0).rotate(orientation); }
 
-bool Transform::isDirty() const { return dirty_matrix; }
+Vector3 Transform::getUp() { return Vector3(0, 1, 0).rotate(orientation); }
 
 std::string Transform::serialize() const {
     auto position_str = std::format("Position: ({}, {}, {})", position.x,
@@ -64,4 +82,24 @@ std::string Transform::serialize() const {
         std::format("Scale: ({}, {}, {})", scale.x, scale.y, scale.z);
 
     return std::format("{}\n{}\n{}", position_str, orientation_str, scale_str);
+}
+
+void Transform::updateLocalMatrix() {
+    Matrix translation = Matrix::translation(local_position);
+    Matrix rotation = Matrix::rotation(local_orientation);
+    Matrix scale_matrix = Matrix::scale(local_scale);
+
+    local_matrix = scale_matrix * rotation * translation;
+
+    dirty_local_matrix = false;
+}
+
+void Transform::updateGlobals() {
+    if (dirty_local_matrix) updateLocalMatrix();
+
+    global_matrix = parent_matrix * local_matrix;
+
+    global_matrix.decompose(position, scale, orientation);
+
+    dirty_globals = false;
 }
--- X11Engine/src/Types/Transform.h
+++ X11Engine/src/Types/Transform.h
@@ -7,36 +7,51 @@
 #include "Quaternion.h"
 #include "Vector3.h"
 
-
 class Transform {
    public:
     Transform();
 
-    void setPosition(const Vector3& position);
-    void setOrientation(const Quaternion& orientation);
-    void setScale(const Vector3& scale);
+    void setLocalPosition(const Vector3& position);
+    void setLocalOrientation(const Quaternion& orientation);
+    void setLocalScale(const Vector3& scale);
 
-    Vector3 getPosition() const;
-    Quaternion getOrientation() const;
-    Vector3 getScale() const;
+    Vector3 getLocalPosition();
+    Quaternion getLocalOrientation();
+    Vector3 getLocalScale();
 
-    Vector3 getForward() const;
-    Vector3 getRight() const;
-    Vector3 getUp() const;
+    Vector3 getPosition();
+    Quaternion getOrientation();
+    Vector3 getScale();
+
+    Vector3 getForward();
+    Vector3 getRight();
+    Vector3 getUp();
 
     Matrix getLocalMatrix();
+    Matrix getGlobalMatrix();
 
-    bool isDirty() const;
+    void setParentTransformMatrix(const Matrix& global_matrix);
 
     std::string serialize() const;
 
    private:
+    void updateGlobals();
+    void updateLocalMatrix();
+
+    Vector3 local_position;
+    Quaternion local_orientation;
+    Vector3 local_scale;
+
     Vector3 position;
     Quaternion orientation;
     Vector3 scale;
 
     Matrix local_matrix;
-    bool dirty_matrix;
+    Matrix global_matrix;
+    Matrix parent_matrix;
+
+    bool dirty_local_matrix;
+    bool dirty_globals;
 };
 
 #endif  // TRANSFORM_H
