972b57d63d0e48352ec40d97ab82e7fe869f93eb
Leer-err <apobylitsa@gmail.com>
2026-01-18 21:47:14 +0300

    Logging for mesh registry

--- X11Engine/src/Assets/Mesh/MeshRegistry.cpp
+++ X11Engine/src/Assets/Mesh/MeshRegistry.cpp
@@ -1,19 +1,28 @@
 #include "MeshRegistry.h"
 
+#include "LoggerFactory.h"
 #include "MeshReader.h"
 
-Mesh MeshRegistry::get(const std::string& name) {
-    auto model = models.find(name);
+MeshRegistry::MeshRegistry()
+    : logger(LoggerFactory::getLogger("MeshRegistry")) {}
 
-    if (model == models.end()) return load(name);
+Mesh MeshRegistry::get(const std::string& name) {
+    auto model = meshes.find(name);
+
+    if (model == meshes.end()) {
+        logger.debug("{} is not found in cache, reading from file", name);
+        return load(name);
+    }
+
+    logger.debug("Found {}", name);
 
     return model->second;
 }
 
 Mesh MeshRegistry::load(const std::string& name) {
-    Mesh loaded = Assets::MeshReader().read(name);
+    Mesh loaded = Assets::MeshReader().readStatic(name);
 
-    models.emplace(name, loaded);
+    meshes.emplace(name, loaded);
 
     return loaded;
 }
--- X11Engine/src/Assets/Mesh/MeshRegistry.h
+++ X11Engine/src/Assets/Mesh/MeshRegistry.h
@@ -3,14 +3,19 @@
 #include <string>
 #include <unordered_map>
 
+#include "Logger.h"
 #include "Mesh.h"
 
 class MeshRegistry {
    public:
+    MeshRegistry();
+
     Mesh get(const std::string& name);
 
    private:
     Mesh load(const std::string& name);
 
-    std::unordered_map<std::string, Mesh> models;
+    std::unordered_map<std::string, Mesh> meshes;
+
+    Logger logger;
 };
