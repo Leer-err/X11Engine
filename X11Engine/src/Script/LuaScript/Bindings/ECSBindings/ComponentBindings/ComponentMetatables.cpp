3f4cb54ee994cb8f7823f71a54d3d6ed6149d615
Leer-err <apobylitsa@gmail.com>
2026-01-23 23:56:38 +0300

    Moved Lua component definition to one file

--- X11Engine/src/Script/LuaScript/Bindings/ECSBindings/ComponentBindings/ComponentMetatables.cpp
+++ X11Engine/src/Script/LuaScript/Bindings/ECSBindings/ComponentBindings/ComponentMetatables.cpp
@@ -2,18 +2,45 @@
 
 #include <lua.hpp>
 
+#include "Entity.h"
 #include "LuaUtility.h"
 #include "MeshBindings/MeshBindings.h"
+#include "StaticMaterial.h"
+#include "StaticMesh.h"
+#include "Transform.h"
 #include "TransformBindings.h"
 #include "TypeIdHelper.h"
 #include "lua.h"
 
-struct Transform;
-struct StaticMesh;
-
 namespace Utility = Engine::Script::Utility;
+namespace StaticModel = Asset::StaticModel;
 
 namespace Engine::Script::Binding::ECS {
+
+void addComponent(lua_State* state, Entity entity, TypeId component_type) {
+    if (component_type == TypeIdHelper::getTypeId<Transform>())
+        entity.add<Transform>();
+    else if (component_type == TypeIdHelper::getTypeId<StaticMesh>())
+        entity.add<StaticMesh>();
+    else if (component_type == TypeIdHelper::getTypeId<StaticModel::Material>())
+        entity.add<StaticMesh>();
+}
+
+int getComponent(lua_State* state, Entity entity, TypeId component_type) {
+    const char* metatable_name;
+    if (component_type == TypeIdHelper::getTypeId<Transform>())
+        metatable_name = TRANSFORM_METATABLE;
+    else if (component_type == TypeIdHelper::getTypeId<StaticMesh>())
+        metatable_name = STATIC_MESH_METATABLE;
+    else
+        return Utility::pushNil(state);
+
+    ComponentId component = entity.getComponentId(component_type);
+    Utility::pushUserData(state, metatable_name, component);
+
+    return 1;
+}
+
 void createComponentTable(lua_State* state) {
     lua_createtable(state, 0, 1);
 
