a1a46dceec7cc6944c12129c36f1eee07058d8f3
Leer-err <apobylitsa@gmail.com>
2026-06-11 16:27:33 +0300

    Adding assets to world

--- src/Scene/Scene.cpp
+++ src/Scene/Scene.cpp
@@ -18,10 +18,6 @@
 #include "Transform.h"
 #include "TransformSystem.h"
 
-// StaticModelData tower_data = {};
-
-// StaticModelData terrain_data = {};
-
 Scene::Scene() {
     setupSystems();
 
@@ -29,27 +25,25 @@ Scene::Scene() {
 
     auto renderer = Graphics::getRenderEngine();
 
-    File::ModelReader reader("./Assets/Tower.fbx");
-    auto vertices = reader.readVertices();
-    auto indices = reader.readIndices();
-
     int width;
     int height;
     int channels;
     unsigned char* data;
 
-    // tower_data.position = {-26, 9, -8};
-    // tower_data.mesh =
-    //     renderer->addMesh(vertices.data(), vertices.size() * sizeof(Vertex),
-    //                       indices.data(), indices.size() * sizeof(uint32_t));
-
-    // unsigned char* data =
-    //     stbi_load("./Assets/tower.png", &width, &height, &channels, 0);
-
-    // tower_data.albedo = renderer->addTexture(data, width, height);
+    OpaqueRenderObjectData tower_data = {};
+    auto tower_reader = File::ModelReader("./Assets/Tower.fbx");
+    auto vertices = tower_reader.readVertices();
+    auto indices = tower_reader.readIndices();
+    tower_data.position = {-26, 9, -8};
+    tower_data.mesh =
+        renderer->addMesh(vertices.data(), vertices.size() * sizeof(Vertex),
+                          indices.data(), indices.size() * sizeof(uint32_t));
+    data = stbi_load("./Assets/tower.png", &width, &height, &channels, 0);
+    tower_data.albedo = renderer->addTexture(data, width, height);
+    renderer->getRenderWorld().addOpaqueObject(tower_data);
 
     OpaqueRenderObjectData gem_data = {};
-    File::ModelReader gem_reader = File::ModelReader("./Assets/Gem2.fbx");
+    auto gem_reader = File::ModelReader("./Assets/Gem2.fbx");
     vertices = gem_reader.readVertices();
     indices = gem_reader.readIndices();
     gem_data.position = {-26, 20, -8};
@@ -57,25 +51,23 @@ Scene::Scene() {
         renderer->addMesh(vertices.data(), vertices.size() * sizeof(Vertex),
                           indices.data(), indices.size() * sizeof(uint32_t));
     data = stbi_load("./Assets/gem2.png", &width, &height, &channels, 0);
-
     gem_data.albedo = renderer->addTexture(data, width, height);
     renderer->getRenderWorld().addOpaqueObject(gem_data);
 
-    // File::ModelReader terrain_reader =
-    // File::ModelReader("./Assets/Island.fbx"); vertices =
-    // terrain_reader.readVertices(); indices = terrain_reader.readIndices();
-    // terrain_data.position = {0, -50, 0};
-    // terrain_data.mesh =
-    //     renderer->addMesh(vertices.data(), vertices.size() * sizeof(Vertex),
-    //                       indices.data(), indices.size() * sizeof(uint32_t));
-    // data =
-    //     stbi_load("./Assets/island_albedo.png", &width, &height, &channels,
-    //     0);
+    OpaqueRenderObjectData terrain_data = {};
+    auto terrain_reader = File::ModelReader("./Assets/Island.fbx");
+    vertices = terrain_reader.readVertices();
+    indices = terrain_reader.readIndices();
+    terrain_data.position = {0, -50, 0};
+    terrain_data.mesh =
+        renderer->addMesh(vertices.data(), vertices.size() * sizeof(Vertex),
+                          indices.data(), indices.size() * sizeof(uint32_t));
+    data =
+        stbi_load("./Assets/island_albedo.png", &width, &height, &channels, 0);
+    terrain_data.albedo = renderer->addTexture(data, width, height);
+    renderer->getRenderWorld().addOpaqueObject(terrain_data);
 
-    // terrain_data.albedo = renderer->addTexture(data, width, height);
-
-    std::shared_ptr<Input::GameInputContext> input =
-        std::make_shared<Input::GameInputContext>();
+    auto input = std::make_shared<Input::GameInputContext>();
     input->addBinding(Input::GameAxes::LookYaw, Input::Axis::MOUSE_X);
     input->addBinding(Input::GameAxes::LookPitch, Input::Axis::MOUSE_Y);
     input->addBinding(Input::GameAxes::MoveForwardBackward,
@@ -105,10 +97,6 @@ void Scene::update(float deltaTime) {
 
     sky.draw();
     world.update(deltaTime);
-
-    // graphics_communicator.send(tower_data);
-    // graphics_communicator.send(gem_data);
-    // graphics_communicator.send(terrain_data);
 }
 
 void Scene::setupSystems() {
